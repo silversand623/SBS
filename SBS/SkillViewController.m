f1dd809588f7934bcd3544366e8cde0be1dcb377
lyn <lyn@lyntekiMac-mini.local>
2015-09-10 14:19:00 +0800

    update image

--- SBS/SkillViewController.m
+++ SBS/SkillViewController.m
@@ -112,7 +112,7 @@
     }
     
     NSDictionary* dic = [self.list objectAtIndex:indexPath.row];
-    cell.numLable.text =[NSString stringWithFormat: @"%d", indexPath.row+1];
+    //cell.numLable.text =[NSString stringWithFormat: @"%d", indexPath.row+1];
     cell.textLable1.text = [dic objectForKey:@"S_name"];
     
     return cell;
